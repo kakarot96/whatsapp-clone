7ae6e5d01a6b29939214b09ecfe28fbc323b75fd
kakarot96 <kunalaggarwal58@gmail.com>
2020-09-10 02:01:50 +0530

    Updates

---
+++ precache-manifest.fd800d9d2fa10507bae27822249024ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296ef61dfc21df4f7070f5347702d556",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "b3bed6bffb3681e71c0f",
+    "url": "/whatsapp-clone/static/css/main.2fee0625.chunk.css"
+  },
+  {
+    "revision": "9409df19f258b98af99f",
+    "url": "/whatsapp-clone/static/js/2.606cf761.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/whatsapp-clone/static/js/2.606cf761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3bed6bffb3681e71c0f",
+    "url": "/whatsapp-clone/static/js/main.e632daed.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/whatsapp-clone/static/media/whatsapp_bg.adbccee0.png"
+  }
+]);
